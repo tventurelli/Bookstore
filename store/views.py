eb3e39ab30027f51187db1af812ca52d6afbeab2
unknown <trevorventurelli@gmail.com>
2024-06-25 15:21:38 -0700

    store app

--- store/views.py
+++ store/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'template.html')
+    return render(request, 'store/template.html')
+
+def store(request):
+    return render(request, 'store/store.html')
